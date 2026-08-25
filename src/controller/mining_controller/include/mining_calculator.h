baa9f94c67562a8ae0c584ed1795faf98fd36583
Killua753 <t2026104239911541@eduxiji.net>
2026-08-25 16:00:05 +0800

    feat(mining_calculator): 完善接口

--- src/controller/mining_controller/include/mining_calculator.h
+++ src/controller/mining_controller/include/mining_calculator.h
@@ -1,20 +1,18 @@
 #pragma once
 
 #include <chrono>
+#include <cstddef>
 
-namespace mud::mining
+class MiningCalculator
 {
-    class MiningCalculator
-    {
-    public:
-        static size_t calculateProductionCount(
-            std::chrono::seconds elapsed,
-            std::chrono::seconds interval
-        );
+public:
+    static std::size_t calculate_production_count(
+        std::chrono::seconds elapsed,
+        std::chrono::seconds interval
+    ) noexcept;
 
-        static std::chrono::seconds calculateRemainingTime(
-            std::chrono::seconds elapsed,
-            std::chrono::seconds interval
-        );
-    };
-}
+    static std::chrono::seconds calculate_consumed_time(
+        std::chrono::seconds elapsed,
+        std::chrono::seconds interval
+    ) noexcept;
+};
