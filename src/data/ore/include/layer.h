bc3042ad8b6c4b9937c0b1c1dd8ccf6fdd475f76
Killua753 <t2026104239911541@eduxiji.net>
2026-08-25 14:49:49 +0800

    feat(mining_layers): 添加照明矿层照明需求

--- src/data/ore/include/layer.h
+++ src/data/ore/include/layer.h
@@ -8,10 +8,16 @@
 
 class Layer
 {
-    struct MiningLayer {
-        std::string layer_name;
+    enum class LightingType {
+        None,   // 不需要照明
+        Torch,  // 火把
+        Lantern // 灯笼
+    };
 
-        size_t mining_level;
+    struct MiningLayer {
+        std::string name;       // 矿区名称
+        size_t mining_level;    // 解锁所需采矿等级
+        LightingType lighting;  // 照明需求
     };
 
     using miningLayerTable = std::unordered_map<std::string, MiningLayer>;
