d9ee5ca21e98548a85b779a954f7218a7427f824
Killua753 <t2026104239911541@eduxiji.net>
2026-08-25 15:59:09 +0800

    refactor(mining_types): 完善挖矿状态

--- src/controller/mining_controller/include/mining_types.h
+++ src/controller/mining_controller/include/mining_types.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <chrono>
+#include <cstddef>
+#include <string>
+
 namespace mud::mining
 {
     enum class MiningStatus
@@ -15,5 +19,38 @@ namespace mud::mining
         Deep,
         Crystal,
         Core
+    };
+
+    enum class MiningSpeed
+    {
+        Core = 1,
+        Crystal = 3,
+        Gold = 5,
+        Silver = 9,
+        Iron = 13,
+    };
+
+    struct MiningTool // 等工具部分完善
+    {
+        MiningSpeed mining_speed;
+        std::chrono::seconds interval{static_cast<size_t>(mining_speed)};
+        double rare_bonus = 0.0;
+    };
+
+    struct MiningContext
+    {
+        std::size_t mining_level = 1;
+
+        bool has_torch = false;
+        bool has_lantern = false;
+
+        MiningTool tool{};
+    };
+
+    struct MiningResult
+    {
+        std::string ore_id;
+        std::size_t experience = 0;
+        std::size_t quantity = 1;
     };
 }
